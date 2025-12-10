82d393eca395ee43b6b4312eabb949295d815701
Stepan Putsiata <sstepanovich546@gmail.com>
2025-12-10 14:22:07 +0300

    done with main featuries

--- features/biometrics/services/deleteDocument.tsx
+++ features/biometrics/services/deleteDocument.tsx
@@ -1,8 +1,5 @@
 import { api } from '@/api';
 export const deleteDocument = async (id: string) => {
-  console.log('====================================');
-  console.log('id ', id);
-  console.log('====================================');
   const { data } = await api.delete(`documents/${id}`);
   const isDelete = data as boolean;
   return isDelete;
--- features/biometrics/services/getHistory.tsx
+++ features/biometrics/services/getHistory.tsx
@@ -2,9 +2,6 @@ import { api } from '@/api';
 import { IDocumentInfo } from '../types';
 export const getHistory = async () => {
   const { data } = await api.get(`documents/history/all`);
-  console.log('history ====================================');
-  console.log(data);
-  console.log('====================================');
   const documents = data as IDocumentInfo[];
   return documents;
 };
